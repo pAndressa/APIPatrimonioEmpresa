dfccc96a991af74046d7bdc35aec9b043a380c18
pAndressa <p.andressasilva@gmail.com>
2020-10-25 16:01:03 -0300

    Acrescentado o exercicio 1 para devolver o maior número de acordo com a informação passada

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Informe número maior do que zero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if(numero > 0)
+            {
+                Console.WriteLine(DevolveMaiorNumero(numero));
+            }
+            else
+            {
+                Console.WriteLine("É necessário digitar um número maior do que zero");
+            }           
+        }
+
+        public static int DevolveMaiorNumero(int numero)
+        {
+            int tamanho = numero.ToString().Length;
+
+            int[] digitos = new int[tamanho];
+            for (int i = 0; i < tamanho; i++)
+            {
+                digitos[i] = numero % 10;
+                numero = numero / 10;
+            }
+
+            Array.Sort(digitos);
+
+            int valorDescendente = 0;
+            for (int i = tamanho - 1; i >= 0; i--)
+            {
+                valorDescendente = valorDescendente * 10 + digitos[i];
+            }
+
+             return valorDescendente;
+        }
+    }
+}
